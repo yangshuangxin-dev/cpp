c80e1b72e4842e9fac98151c8c74331f82f1d37b
yangshuangxin <yangshuangxin@qq.com>
2021-01-19 23:55:34 +0800

    20210119

---
+++ HuaWei/合法mac地址/合法mac地址.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+public:
+    int GetMacNum(string ins)
+    {
+        set<string> s;
+        for(int i=0;i+17<=ins.size();i++)
+        {
+            string tmp=ins.substr(i,17);
+            if(IsValid(tmp))
+            {
+                Reshape(tmp);
+                s.insert(tmp);
+            }
+        }
+
+        return s.size();
+    }
+
+    void Reshape(string &s)
+    {
+        for(auto &ch:s)
+        {
+            if(ch==':')
+            {
+                ch='-';
+            }
+            ch=toupper(ch);
+        }
+    }
+
+    bool IsHex(char ch)
+    {
+        if(ch>='0'&&ch<='9')
+        {
+            return true;
+        }
+
+        if(ch>='a'&&ch<='f')
+        {
+            return true;
+        }
+
+        if(ch>='A'&&ch<='F')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool IsValid(string s)
+    {
+        int tmp=max(count(s.begin(),s.end(),'-'),count(s.begin(),s.end(),':'));
+        if(tmp!=5)
+        {
+            return false;
+        }
+
+        for(int i=0;i<s.size();i++)
+        {
+            if((i+1)%3!=0&&!IsHex(s[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+};
+
+inline string ReadLine()
+{
+    string line;
+    getline(cin,line);
+    return line;
+}
+
+int main()
+{
+    string instr=ReadLine();
+    Solution solu;
+    int res=solu.GetMacNum(instr);
+    cout<<res<<endl;
+    return 0;
+}
